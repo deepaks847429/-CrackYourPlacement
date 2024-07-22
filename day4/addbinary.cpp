81a8aa6fa3616ed89c9e44e5358794628aced5ca
Deepak kumar <deepaks9631342845@gmail.com>
2024-07-22 10:25:26 +0530

    day4

---
+++ day4/addbinary.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        std::string result = "";
+        int carry = 0;
+        int i = a.size() - 1;
+        int j = b.size() - 1;
+
+        while (i >= 0 || j >= 0 || carry) {
+            int sum = carry;
+            if (i >= 0)
+                sum += a[i--] - '0';
+            if (j >= 0)
+                sum += b[j--] - '0';
+            result += (sum % 2) + '0';
+            carry = sum / 2;
+        }
+
+        std::reverse(result.begin(), result.end());
+        return result;
+    }
+};
